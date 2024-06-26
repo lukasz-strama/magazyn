e275fc546483fd6b9909e573afeb6d95f1791bb7
lukasz-strama <lukasz-strama@outlook.com>
2024-06-26 16:34:59 +0000

    Deploying to gh-pages from @ lukasz-strama/pbl-magazyn@74d2f5012d48412e32b7c48cec4c39097ee42540 🚀

--- search/all_a.js
+++ search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['order_0',['Order',['../d5/d6b/classOrder.html',1,'Order'],['../d5/d6b/classOrder.html#aa5256eb4e8f0d82ce676f9343493aa27',1,'Order::Order()']]],
-  ['orderhistory_1',['OrderHistory',['../db/d68/classOrderHistory.html',1,'']]]
+  ['orderexists_1',['orderExists',['../db/d68/classOrderHistory_a0bda184fedcb74b6cc5ffda699b2670e.html#a0bda184fedcb74b6cc5ffda699b2670e',1,'OrderHistory']]],
+  ['orderhistory_2',['OrderHistory',['../db/d68/classOrderHistory.html',1,'']]]
 ];
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['order_0',['Order',['../d5/d6b/classOrder.html#aa5256eb4e8f0d82ce676f9343493aa27',1,'Order']]]
+  ['order_0',['Order',['../d5/d6b/classOrder.html#aa5256eb4e8f0d82ce676f9343493aa27',1,'Order']]],
+  ['orderexists_1',['orderExists',['../db/d68/classOrderHistory_a0bda184fedcb74b6cc5ffda699b2670e.html#a0bda184fedcb74b6cc5ffda699b2670e',1,'OrderHistory']]]
 ];
