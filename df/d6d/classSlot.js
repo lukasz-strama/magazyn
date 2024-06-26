f120d8aa06388e1315db7c6cef618178693d87d3
danielsledz <45215297+danielsledz@users.noreply.github.com>
2024-06-26 15:54:06 +0000

    Deploying to gh-pages from @ lukasz-strama/pbl-magazyn@d72961f9c99ac23fcfb914795f494d126c2c6d12 🚀

--- df/d6d/classSlot.js
+++ df/d6d/classSlot.js
@@ -5,7 +5,6 @@ var classSlot =
     [ "isSlotFull", "df/d6d/classSlot_a1695b6f420ffa4eadfdfdae53fd2470c.html#a1695b6f420ffa4eadfdfdae53fd2470c", null ],
     [ "removePackage", "df/d6d/classSlot_a256bfcbe344bf5c9876ca0924b8a5d50.html#a256bfcbe344bf5c9876ca0924b8a5d50", null ],
     [ "setLocation", "df/d6d/classSlot.html#a1c8d563f03bb06abea8ef033cab79d4e", null ],
-    [ "isFull", "df/d6d/classSlot.html#af3eac80ff2566b97503c9d342d1578ac", null ],
     [ "items", "df/d6d/classSlot.html#a2e9b4571aa833d9a96b74f6914262f7c", null ],
     [ "location", "df/d6d/classSlot.html#af505f2f77576cc3936d1ba120c37f4d3", null ],
     [ "maxSize", "df/d6d/classSlot.html#ad74903dcd55644acd428433f957d874c", null ]
--- search/variables_0.js
+++ search/variables_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['isfull_0',['isFull',['../df/d6d/classSlot.html#af3eac80ff2566b97503c9d342d1578ac',1,'Slot']]],
-  ['items_1',['items',['../df/d6d/classSlot.html#a2e9b4571aa833d9a96b74f6914262f7c',1,'Slot']]]
+  ['items_0',['items',['../df/d6d/classSlot.html#a2e9b4571aa833d9a96b74f6914262f7c',1,'Slot']]]
 ];
